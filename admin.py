e5442b1962954212146425b3708269c892d5170f
Andrew Mutz <andrew.mutz@gmail.com>
2015-04-13 00:08:55 +0000

    adding jruby support

--- admin.py
+++ admin.py
@@ -481,9 +481,19 @@ user_sudo passenger start -d --no-compile-runtime\
  || error_exit 'Failed to start passenger'
 """,
             'puma': """# Configure the app to serve static assets
-# Start up WEBrick (or whatever server is installed)
 echo -e "\ngem 'puma' " >> /home/ec2-user/app/Gemfile
 cd /home/ec2-user/app
+if [ '{RubyVM}' == 'JRuby' ]; then
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  curl -sSL https://get.rvm.io | bash -s stable
+  echo "source /home/ec2-user/.profile" >> /home/ec2-user/.bash_profile
+  source /home/ec2-user/.profile
+  rvm install jruby-1.7.19
+  rvm --default use jruby-1.7.19
+  sudo yum install mysql-connector-java
+  echo "\$CLASSPATH ||= [] " >> config/application.rb;
+  echo "\$CLASSPATH << '/usr/share/java/mysql-connector-java.jar'" >> config/application.rb;
+fi
 user_sudo "bundle install"
 user_sudo RAILS_SERVE_STATIC_FILES=true bundle exec puma -t {ThreadParallelism} -w {ProcessParallelism} -p 3000 -d || error_exit 'Failed to start rails server'
 """,
@@ -653,6 +663,8 @@ fi
           self.add_parameter('ProcessParallelism', default='1',
                            description='The number of worker processes.')
           self.add_parameter('ThreadParallelism', default='1',
+                           description='The number of threads within each worker processes.')
+          self.add_parameter('RubyVM', default='MRI', allowed=['MRI', 'JRuby'],
                            description='The number of threads within each worker processes.')
         
 
@@ -1061,7 +1073,7 @@ def main():
                                      passenger=args['--passenger'],
                                      puma=args['--puma'])
     elif args['cftemplate-update-all']:
-        bit_pos = ['passenger', 'multi', 'memcached']
+        bit_pos = ['passenger', 'multi', 'memcached', 'puma']
         for i in range(2 ** len(bit_pos)):
             kwargs = {'app_ami': None}
             for bit, argument in enumerate(bit_pos):
