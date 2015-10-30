c677743817ff0e6b0a235f94091863e8569762ed
Bryce Boe <bbzbryce@gmail.com>
2015-10-30 16:34:51 -0700

    Extract CMD functions into their own functions.

--- scalable_admin/command_line.py
+++ scalable_admin/command_line.py
@@ -20,13 +20,8 @@ from . import configure_github_team
 from . import parse_config
 
 
-def main():
-    """Enter admin.py."""
-    args = docopt(__doc__)
-
-    parse_config()
-
-    # Replace spaces with hyphens in team names
+def clean_team_names(args):
+    """Replace spaces with hyphens in team names."""
     if args['TEAM']:
         if isinstance(args['TEAM'], list):
             for i, item in enumerate(args['TEAM']):
@@ -34,50 +29,88 @@ def main():
         else:
             args['TEAM'] = args['TEAM'].strip().replace(' ', '-')
 
-    if args['aws']:
-        for team in args['TEAM']:
-            retval = AWS().configure(team)
-            if retval:
-                return retval
-        return 0
-    elif args['aws-cleanup']:
-        return AWS().cleanup()
-    elif args['aws-purge']:
-        for team in args['TEAM']:
-            retval = AWS().purge(team)
-            if retval:
-                return retval
-    elif args['aws-update-all']:
-        aws = AWS()
-        for team in aws.team_to_security_group():
-            retval = aws.configure(team)
-            if retval:
-                return retval
-        return 0
-    elif args['cftemplate']:
+
+def cmd_aws(args):
+    """Handle the aws command."""
+    for team in args['TEAM']:
+        retval = AWS().configure(team)
+        if retval:
+            return retval
+    return 0
+
+
+def cmd_aws_cleanup(_):
+    """Handle the aws-cleanup command."""
+    return AWS().cleanup()
+
+
+def cmd_aws_purge(args):
+    """Handle the aws-purge command."""
+    for team in args['TEAM']:
+        retval = AWS().purge(team)
+        if retval:
+            return retval
+    return 0
+
+
+def cmd_aws_update_all(args):
+    """Handle the aws-update-all command."""
+    aws = AWS()
+    for team in aws.team_to_security_group():
+        retval = aws.configure(team)
+        if retval:
+            return retval
+    return 0
+
+
+def cmd_cftemplate(args):
+    """Handle the cftemplate command."""
+    cf = CFTemplate(test=not args['--no-test'])
+    if args['tsung']:
+        return cf.generate_tsung()
+    return cf.generate_stack(app_ami=args['--ami'],
+                             memcached=args['--memcached'],
+                             multi=args['--multi'], puma=args['--puma'])
+
+
+def cmd_cftemplate_update_all(args):
+    """Handle the cftemplate-update-all command."""
+    bit_pos = ['memcached', 'puma', 'multi']
+    for i in range(2 ** len(bit_pos)):
+        kwargs = {'app_ami': None}
+        for bit, argument in enumerate(bit_pos):
+            kwargs[argument] = bool(i & 2 ** bit)
         cf = CFTemplate(test=not args['--no-test'])
-        if args['tsung']:
-            return cf.generate_tsung()
-        else:
-            return cf.generate_stack(app_ami=args['--ami'],
-                                     memcached=args['--memcached'],
-                                     multi=args['--multi'],
-                                     puma=args['--puma'])
-    elif args['cftemplate-update-all']:
-        bit_pos = ['memcached', 'puma', 'multi']
-        for i in range(2 ** len(bit_pos)):
-            kwargs = {'app_ami': None}
-            for bit, argument in enumerate(bit_pos):
-                kwargs[argument] = bool(i & 2 ** bit)
-            cf = CFTemplate(test=not args['--no-test'])
-            retval = cf.generate_stack(**kwargs)
-            if retval:
-                return retval
-        return 0
-    elif args['gh']:
-        team = args['TEAM']
-        team = team[0] if isinstance(team, list) else team
-        return configure_github_team(team_name=team,
-                                     user_names=args['USER'])
+        retval = cf.generate_stack(**kwargs)
+        if retval:
+            return retval
+    return 0
+
+
+def cmd_gh(args):
+    """Handle the gh command."""
+    team = args['TEAM']
+    team = team[0] if isinstance(team, list) else team
+    return configure_github_team(team_name=team, user_names=args['USER'])
+
+
+def main():
+    """Provide the entrance point for the scalable_admin command."""
+    args = docopt(__doc__)
+
+    parse_config()
+    clean_team_names(args)
+
+    commands = {'aws': cmd_aws,
+                'aws-cleanup': cmd_aws_cleanup,
+                'aws-purge': cmd_aws_purge,
+                'aws-update-all': cmd_aws_update_all,
+                'cftemplate': cmd_cftemplate,
+                'cftemplate-update-all': cmd_cftemplate_update_all,
+                'gh': cmd_gh}
+
+    for command_name in commands:
+        if args[command_name]:
+            return commands[command_name](args)
     else:
         raise Exception('Invalid state')
