13a859b3858721d0f94009b95b2d7d8608f46e66
Andrew Mutz <andrew.mutz@gmail.com>
2019-09-17 08:02:22 -0700

    Adding two additional admin tools

---
+++ cs291_cleanup.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from datetime import datetime, timedelta, tzinfo
+import botocore.session
+import sys
+
+
+REGION = "us-west-2"
+
+
+class UTC(tzinfo):
+    dst = lambda x, y: timedelta(0)
+    tzname = lambda x, y: "UTC"
+    utcoffset = lambda x, y: timedelta(0)
+
+
+NOW = datetime.now(UTC())
+
+
+def stacks(cf):
+    next_token = True
+    while next_token:
+        params = {}
+        if isinstance(next_token, str):
+            params["NextToken"] = next_token
+        response = cf.list_stacks(**params)
+        next_token = response.get("NextToken", None)
+        for stack in response["StackSummaries"]:
+            if stack["StackStatus"] != "DELETE_COMPLETE":
+                yield stack
+
+
+def is_stale_stack(stack):
+    # Any non-elasticbeanstalk stack running for 5 hours
+    return not stack["StackName"].startswith("awseb-e") and NOW - stack[
+        "CreationTime"
+    ] >= timedelta(minutes=300)
+
+
+def is_delete_failed_stack(stack):
+    return stack["StackStatus"] == "DELETE_FAILED"
+
+
+def active_databases(rds):
+    dbs = set()
+    for instance in rds.describe_db_instances()["DBInstances"]:
+        dbs.add(instance["DBInstanceIdentifier"])
+        if instance["DBInstanceStatus"] == "available":
+            pass
+        elif instance["DBInstanceStatus"] not in ["backing-up", "creating", "deleting"]:
+            print("Unhandled RDS state: {}".format(instance["DBInstanceStatus"]))
+    return dbs
+
+
+def delete_snapshots(rds):
+    deleted = 0
+    for snapshot in rds.describe_db_snapshots()["DBSnapshots"]:
+        if snapshot["Status"] == "creating":
+            continue
+        elif snapshot["SnapshotType"] == "manual":
+            rds.delete_db_snapshot(
+                DBSnapshotIdentifier=snapshot["DBSnapshotIdentifier"]
+            )
+            deleted += 1
+        elif snapshot["SnapshotType"] != "automated":
+            import pprint
+
+            pprint.pprint(snapshot)
+    if deleted > 0:
+        print("Deleted snapshots: {}")
+
+
+def main():
+    aws = botocore.session.Session(profile="cs291")
+
+    print("Running")
+    rds = aws.create_client("rds", REGION)
+    delete_snapshots(rds)
+    dbs = active_databases(rds)
+
+    cf = aws.create_client("cloudformation", REGION)
+    for stack in stacks(cf):
+        resources = {
+            x["LogicalResourceId"]: x
+            for x in cf.describe_stack_resources(StackName=stack["StackName"])[
+                "StackResources"
+            ]
+        }
+        if "AWSEBRDSDatabase" in resources:
+            rds_id = resources["AWSEBRDSDatabase"]["PhysicalResourceId"]
+            if rds_id in dbs:
+                dbs.remove(rds_id)
+
+        params = {}
+        if is_stale_stack(stack):
+            pass
+        elif is_delete_failed_stack(stack):
+            reason = stack["StackStatusReason"]
+            index = reason.rfind("[")
+            logical_ids = [x.strip() for x in reason[index + 1 : -3].split(",")]
+            params["RetainResources"] = logical_ids
+        else:
+            continue
+        print(
+            "Created: {} Deleting {} ".format(
+                NOW - stack["CreationTime"], stack["StackName"]
+            )
+        )
+        cf.delete_stack(StackName=stack["StackName"], **params)
+
+    # Remove orphaned databases
+    for db in dbs:
+        rds.delete_db_instance(DBInstanceIdentifier=db, SkipFinalSnapshot=True)
+
+    # Terminate any deployment that hasn't been updated within an hour
+    eb = aws.create_client("elasticbeanstalk", REGION)
+    for deployment in eb.describe_environments()["Environments"]:
+        if (
+            deployment["Status"].startswith("Terminat")
+            or deployment["Status"] == "Launching"
+            or NOW - deployment["DateUpdated"] < timedelta(minutes=110)
+        ):
+            continue
+        print(
+            "Last Update: {} Terminating {} ({})".format(
+                NOW - deployment["DateUpdated"],
+                deployment["EnvironmentName"],
+                deployment["Status"],
+            )
+        )
+        eb.terminate_environment(EnvironmentId=deployment["EnvironmentId"])
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ prepare_accounts.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+CREDENTIALS_PATH="$(dirname $0)/credentials"
+
+
+add_files() {
+    teamname=$1
+    for extension in _key.txt .pem .txt; do
+        filename=$teamname$extension
+        source_path="$CREDENTIALS_PATH/$filename"
+        destination_path="/home/$teamname/$filename"
+        sudo cp $source_path $destination_path
+        sudo chown $teamname:$teamname $destination_path
+    done
+}
+
+
+create_account() {
+    teamname=$1
+    id -u $teamname >/dev/null 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo -n "Creating user $teamname... "
+        sudo useradd $teamname
+        echo "success"
+    fi
+}
+
+
+set_authorized_key() {
+    teamname=$1
+    ssh_key_path="$CREDENTIALS_PATH/$teamname.pem"
+    public_key=$(ssh-keygen -y -f $ssh_key_path)
+    ssh_path="/home/$teamname/.ssh"
+    authorized_keys_path="$ssh_path/authorized_keys"
+    sudo mkdir -p $ssh_path
+    sudo chown $teamname:$teamname $ssh_path
+    sudo chmod 0700 $ssh_path
+    echo $public_key | sudo tee $authorized_keys_path > /dev/null
+    sudo chown $teamname:$teamname $authorized_keys_path
+}
+
+
+for pempath in $(ls $CREDENTIALS_PATH/*.pem); do
+    pemfile=$(basename $pempath)
+    team="${pemfile%.*}"
+
+    create_account $team
+    add_files $team
+    set_authorized_key $team
+done
