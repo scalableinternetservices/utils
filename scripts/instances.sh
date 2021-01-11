bb0e00b8be2c732f92ca286813cca77c5f7e5f17
Bryce Boe <bbzbryce@gmail.com>
2021-01-10 19:39:12 -0800

    Move scripts to ec2.cs291.com repo

--- scripts/instances.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-aws ec2 describe-instances \
-    | jq '[.Reservations[].Instances[] |
-          {instance_id: .InstanceId,
-           instance_type: .InstanceType,
-           key: .KeyName,
-	   name: (.Tags | map(select(.Key == "Name")))[0].Value,
-           state: .State.Name}]'
